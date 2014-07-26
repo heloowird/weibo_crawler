edca6b54379027036ca430d36352522ed9b4ffe6
heloowird <heloowird@gmail.com>
2014-07-26 19:49:11 +0800

    编译成windows可执行窗口程序

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+# coding: utf-8
 from distutils.core import setup
 import py2exe
 
+## 进入控制台，输入python setup.py py2exe，即可生成window窗口程序
 setup(
     console=[{'script': 'collectWeiboDataByKeyword.py'}],
     options={
